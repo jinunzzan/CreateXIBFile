9fbb6f19255a8c13e7c53742d0d26da5f6c775cb
jinunzzan <lovebox6607@gmail.com>
2022-10-21 02:01:51 +0900

    XIB 파일로 테이블 뷰 셀 만들기!

--- CreateXIBFile/ViewController.swift
+++ CreateXIBFile/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    var arr = ["a", "b", "c", "d"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let nibName = UINib(nibName: "TableViewCell", bundle: nil) //nibName에는 TableViewCell의 파일 이름을
+        tableView.register(nibName, forCellReuseIdentifier: "Cell") //forCellReuseIdentifier에는 cell의 identifier을 쓴다
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
+        cell.lable.text = arr[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
